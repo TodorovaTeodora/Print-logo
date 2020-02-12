dfb5d248affa099654706f4e58fa91e96db8842a
TodorovaTeodora <57221159+TodorovaTeodora@users.noreply.github.com>
2020-02-12 09:37:12 +0200

    Add files via upload

---
+++ ConcatClass.java
@@ -0,0 +1,50 @@
+public class Concatenation {
+   private int inputNumber;
+   private String symbol;
+   private String space;
+
+    public Concatenation(int inputNumber, String symbol, String space) {
+        this.inputNumber = inputNumber;
+        this.symbol = symbol;
+        this.space = space;
+    }
+    public void print(int inputNumber, String symbol, String space) {
+         final int range = (inputNumber + 1) / 2;
+        for (int i = 0; i < range; i++) {
+            StringBuilder builder = new StringBuilder();
+            String concatenationUpper = space.repeat(inputNumber - i)
+                    + symbol.repeat(inputNumber + 2 * i)
+                    + space.repeat(inputNumber - 2 * i)
+                    + symbol.repeat(inputNumber + 2 * i)
+                    + space.repeat(inputNumber - i)
+                    + space.repeat(inputNumber - i)
+                    + symbol.repeat(inputNumber + 2 * i)
+                    + space.repeat(inputNumber - 2 * i)
+                    + symbol.repeat(inputNumber + 2 * i)
+                    + space.repeat(inputNumber - i);
+            builder.append(concatenationUpper);
+            System.out.println(builder.toString());
+
+        }
+        for (int i = 0; i < range; i++) {
+            StringBuilder builder = new StringBuilder();
+            String concatenationLower = space.repeat(inputNumber / 2 - i)
+                    + symbol.repeat(inputNumber)
+                    + space.repeat(1 + 2 * i)
+                    + symbol.repeat((inputNumber * 2) - 1 - 2 * i)
+                    + space.repeat(1 + 2 * i)
+                    + symbol.repeat(inputNumber)
+                    + space.repeat(inputNumber - 1 - 2 * i)
+                    + symbol.repeat(inputNumber)
+                    + space.repeat(1 + 2 * i)
+                    + symbol.repeat((inputNumber * 2) - 1 - 2 * i)
+                    + space.repeat(1 + 2 * i)
+                    + symbol.repeat(inputNumber)
+                    + space.repeat(inputNumber / 2 - i);
+            builder.append(concatenationLower);
+            System.out.println(builder.toString());
+
+        }
+    }
+
+}
---
+++ Printer.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class PrintLogo {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int inputNumber = Integer.parseInt(scan.nextLine());
+        String symbol = "*";
+        String space = "-";
+
+        // inputNumber must be > 2 and an odd number:
+
+        if (inputNumber > 2 && inputNumber % 2 != 0) {
+            // create object
+            Concatenation printer = new Concatenation(inputNumber, symbol, space);
+            // print
+            printer.print(inputNumber, symbol, space);
+
+        }
+
+    }
+
+}
